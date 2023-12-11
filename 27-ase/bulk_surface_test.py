f6f0dfcbe85eb30c0396ffd755ea6f4ac7f646e9
YSL <3242809681@qq.com>
2023-12-11 20:08:26 +0800

    add bulk_surface_test.py

---
+++ 27-ase/bulk_surface_test.py
@@ -0,0 +1,13 @@
+from ase.build import (
+    bcc100,
+    bcc110,
+    bcc111,
+    diamond100,
+    diamond111,
+    fcc100,
+    fcc110,
+    fcc111,
+    graphene,
+    graphene_nanoribbon,
+    nanotube,
+)
